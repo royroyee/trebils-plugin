b7aad125928a2368d97a3bc0f7a1314c10d1a1dc
royroyee <kim131325@naver.com>
2023-10-11 14:58:30 +0900

    Add gke terraform module

---
+++ gcp/gke/terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+project_id = "boanlab"
+region     = "asia-northeast3"
+location = "asia-northeast3-b"
---
+++ gcp/gke/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+variable "location" {
+  description = "location"
+}
